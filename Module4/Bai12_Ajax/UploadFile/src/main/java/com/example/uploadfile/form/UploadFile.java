1326e31a3e3875f1ff72abd8357c6c3ab9973bac
vulinh1704 <truongdangvulinh1704@gmail.com>
2022-06-28 11:06:49 +0700

    28/6 11:06

---
+++ Module4/Bai12_Ajax/UploadFile/src/main/java/com/example/uploadfile/form/UploadFile.java
@@ -0,0 +1,8 @@
+package com.example.uploadfile.form;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadFile {
+    private String description;
+    private MultipartFile[] files;
+}
